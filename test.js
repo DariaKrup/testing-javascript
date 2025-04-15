b6ea452cdc5af93d1d39797998ee9e42c9a45d00
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2025-04-15 15:54:12 +0200

    Add a test and a digit to the beginning of the namespace

--- test.js
+++ test.js
@@ -2,8 +2,14 @@
 // var assert = require('chai').assert;
 // var square = require('./implementation');
 
-describe('square', function() {
+describe('1square', function() {
   it('returns the square of a number', function() {
     assert.equal(square(2), 4);
+  });
+});
+
+describe('2square', function() {
+  it('returns the square of a number', function() {
+    assert.equal(square(3), 9);
   });
 });
