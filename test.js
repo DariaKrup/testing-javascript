56a3c031ceef9e6fdeda0831c9a7f961795e8a5c
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2025-04-15 16:43:09 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,14 +2,16 @@
 const assert = require('assert'); 
 const { square } = require('./implementation');
 
-describe('1square', function() {
-  it('returns the square of a number', function() {
-    assert.equal(square(2), 4);
+describe('SquareFunction', function () {
+  describe('1square', function() {
+    it('returns the square of a number', function() {
+      assert.equal(square(2), 4);
+    });
   });
-});
-
-describe('2square', function() {
-  it('returns the square of a number', function() {
-    assert.equal(square(3), 9);
+  
+  describe('2square', function() {
+    it('returns the square of a number', function() {
+      assert.equal(square(3), 9);
+    });
   });
 });
